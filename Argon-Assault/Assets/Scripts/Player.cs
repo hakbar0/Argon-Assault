32584cb5a6b385b71afdeac77df77a033541bb6e
Haseeb Akbar <haseeb2000@hotmail.com>
2018-06-03 15:28:19 +0100

    Ship able to move in x and y direction.

--- Argon-Assault/Assets/Scripts/Player.cs
+++ Argon-Assault/Assets/Scripts/Player.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    [Tooltip("In ms^-1")][SerializeField] float speed = 15f;
+    [Tooltip("In m")] [SerializeField] float xRange = 3f;
+    [Tooltip("In m")] [SerializeField] float yRange = 2f;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
-	}
+	void Update ()
+    {
+        horizontalMovement();
+        verticalMovement();
+    }
+
+    private void horizontalMovement()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = speed * xThrow * Time.deltaTime;
+        float rawNewXPostion = transform.localPosition.x + xOffsetThisFrame;
+        float clampedXPos = Mathf.Clamp(rawNewXPostion, -xRange, xRange);
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void verticalMovement()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetThisFrame = speed * yThrow * Time.deltaTime;
+        float rawNewYPostion = transform.localPosition.y + yOffsetThisFrame;
+        float clampedYPos = Mathf.Clamp(rawNewYPostion, -yRange, yRange);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+
 }
