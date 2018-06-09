0a81a75fe502267c7cc2b00e05b4e7a969ff5728
Haseeb Akbar <haseeb2000@hotmail.com>
2018-06-09 18:04:11 +0100

    Reloads scene when player dies.

--- Argon-Assault/Assets/Scripts/CollisionHandler.cs
+++ Argon-Assault/Assets/Scripts/CollisionHandler.cs
@@ -2,17 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour {
+
+    [Tooltip("Seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("Particle effect on player")][SerializeField] GameObject deathFx;
 
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFx.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
-        print("Player has died");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
